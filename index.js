d406ae98b2d420f3450eacf45365f8eca2ff970e
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-25 09:45:57 -0700

    removed push method from mapClone and wrote array test for filter clone

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const fib = [3, 5, 8, 13, 21, 34];
 
+// actual functionaliy array methods
+const maped = fib.map(num => num * 2); // map functionality
+const even = fib.filter(num => num % 2 === 0); // filter functionality
+
+console.log(maped);
+console.log(even);
+
 function mapClone(arr, callback) {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
-    const num = arr[i];
-    newArr.push(callback(num));
+    newArr[i] = callback(arr[i]);
   }
   return newArr;
+}
+
+function filterClone(arr, callback) {
 }
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -1,8 +1,27 @@
-const { fib, mapClone } = require('./index');
+const { fib, mapClone, filterClone } = require('./index');
 
-describe('testing array method clones', () => {
+describe('testing mapClone function', () => {
+  it('returns an array', () => {
+    const result = mapClone(fib, num => num * 2);
+    expect(result).toEqual(expect.any(Array));
+  });
+
   it('takes an array and a callback and returns new array with the new values of each called callback', () => {
     const result = mapClone(fib, (num) => num * num);
     expect(result).toEqual([9, 25, 64, 169, 441, 1156]);
   });
+
+  it('checks to make sure both arrays are the same length', () => {
+    const result = mapClone(fib, (num) => num * num);
+    expect(result.length).toEqual(fib.length);
+    // expect(result).toHaveLength(fib.length) // same as above
+  });
 });
+
+describe('testing filterClone function', () => {
+  it('returns an array', () => {
+    const result = filterClone(fib, num => num % 2 === 0);
+    expect(result).toEqual(expect.any(Array));
+  });
+});
+
